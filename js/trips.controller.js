cb668c96f6653bee8cd958748f6597a17c9dc942
Antonio Rhee <antoniolrhee@gmail.com>
2016-12-20 16:49:03 -0800

    edits new and edit forms to include state selector

--- js/trips.controller.js
+++ js/trips.controller.js
@@ -26,6 +26,11 @@
     var vm = this;
     vm.newTrip = {};
     vm.addTrip = addTrip;
+    vm.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
+    'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
+    'WY').split(' ').map(function(state) {
+        return {abbrev: state};
+      });
 
     function addTrip() {
       if(vm.newTrip.bookmarks && typeof vm.newTrip.bookmarks === 'string') {
