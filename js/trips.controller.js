3531657a3f01520aa9dfe3e96e0478e028584570
Antonio Rhee <antoniolrhee@gmail.com>
2017-01-17 17:52:29 -0800

    adds functionality to add events to google calendar from the app

--- js/trips.controller.js
+++ js/trips.controller.js
@@ -9,7 +9,7 @@
 
   TripsListController.$inject = ['TripsResource', 'authService'];
   TripsNewController.$inject = ['TripsResource', '$state'];
-  TripsShowController.$inject = ['TripsResource', '$stateParams', '$state', '$http', 'NgMap', '$sce', '$scope'];
+  TripsShowController.$inject = ['TripsResource', '$stateParams', '$state', '$http', 'NgMap', '$sce', '$scope', '$mdDialog'];
   TripsEditController.$inject = ['TripsResource', '$state', '$stateParams'];
 
 //
@@ -31,7 +31,7 @@
   function TripsNewController(TripsResource, $state) {
     var vm = this;
     vm.newTrip = {};
-    // there is currently an error about the instance of the datepicker, the code below might help
+    // there is currently a console error about the instance of the datepicker, the code below might help
     // vm.startDate = new Date();
     // vm.endDate = new Date();
     vm.addTrip = addTrip;
@@ -56,7 +56,7 @@
 //
 // TRIPS SHOW CONTROLLER
 //
-  function TripsShowController(TripsResource, $stateParams, $state, $http, NgMap, $sce, $scope) {
+  function TripsShowController(TripsResource, $stateParams, $state, $http, NgMap, $sce, $scope, $mdDialog) {
     var vm = this;
     // for google calendar api
     vm.handleAuthClick = handleAuthClick;
@@ -65,6 +65,34 @@
     vm.calendars = [];
     vm.startDay = new Date();
     vm.getCalendarEvents = getCalendarEvents;
+    vm.createCalendarEvent = createCalendarEvent;
+    vm.createEventDialog = createEventDialog;
+    vm.eventHours = ["00:00", "00:15", "00:30", "00:45",
+    "01:00", "01:15", "01:30", "01:45",
+    "02:00", "02:15", "02:30", "02:45",
+    "03:00", "03:15", "03:30", "03:45",
+    "04:00", "04:15", "04:30", "04:45",
+    "05:00", "05:15", "05:30", "05:45",
+    "06:00", "06:15", "06:30", "06:45",
+    "07:00", "07:15", "07:30", "07:45",
+    "08:00", "08:15", "08:30", "08:45",
+    "09:00", "09:15", "09:30", "09:45",
+    "10:00", "10:15", "10:30", "10:45",
+    "11:00", "11:15", "11:30", "11:45",
+    "12:00", "12:15", "12:30", "12:45",
+    "13:00", "13:15", "13:30", "13:45",
+    "14:00", "14:15", "14:30", "14:45",
+    "15:00", "15:15", "15:30", "15:45",
+    "16:00", "16:15", "16:30", "16:45",
+    "17:00", "17:15", "17:30", "17:45",
+    "18:00", "18:15", "18:30", "18:45",
+    "19:00", "19:15", "19:30", "19:45",
+    "20:00", "20:15", "20:30", "20:45",
+    "21:00", "21:15", "21:30", "21:45",
+    "22:00", "22:15", "22:30", "22:45",
+    "23:00", "23:15", "23:30", "23:45", "24:00"
+    ]
+
     // for overall functionality of the trips show page
     vm.trip = {};
     vm.deleteTrip = deleteTrip;
@@ -77,7 +105,7 @@
     vm.searchAddress = searchAddress;
 
     var CLIENT_ID = '1055578100655-vov0la7q2vr9acqesmj5dvb5t9fv14tp.apps.googleusercontent.com';
-    var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+    var SCOPES = ["https://www.googleapis.com/auth/calendar"];
 
     function handleAuthResult(authResult) {
       // big issue: when i sign in and authorize a calendar, signing in to a diff acc loads the same cal
@@ -140,6 +168,55 @@
           }
         });
         $scope.$apply();
+      });
+    }
+
+    function createEventDialog(ev) {
+      if(vm.selectedCal == null) {
+        angular.element(document.querySelector('#open-event-form-button')).parent().append("<p style='font-weight: bold; font-size: 14px' id='select-cal-warning'>Please select a calendar.</p>");
+      } else {
+        angular.element(document.querySelector('#select-cal-warning')).html('');
+        $mdDialog.show({
+          controller: TripsShowController,
+          contentElement: '#event-dialog',
+          parent: angular.element(document.body),
+          targetEvent: ev,
+          clickOutsideToClose: true
+        });
+      }
+    };
+
+    function createCalendarEvent() {
+      var event = {
+        'summary': vm.summary,
+        'location': vm.location,
+        'description': vm.description,
+        'start': {
+          'dateTime': new Date((vm.startDateTime).toString().split('').slice(0, 15).join('') + ' @ ' + vm.startHour),
+          'timeZone': 'America/Los_Angeles'
+        },
+        'end': {
+          'dateTime': new Date((vm.endDateTime).toString().split('').slice(0, 15).join('') + ' @ ' + vm.endHour),
+          'timeZone': 'America/Los_Angeles'
+        },
+        'attendees': [],
+        'reminders': {
+          'useDefault': false
+        }
+      };
+      console.log(event.start.dateTime);
+      console.log(event.end.dateTime);
+
+      // '2017-01-21T09:00:00-10:00'
+
+      var request = gapi.client.calendar.events.insert({
+        'calendarId': vm.selectedCal,
+        'resource': event
+      });
+
+      request.execute(function(event) {
+        $mdDialog.hide();
+        // alert('Event created: ' + event.htmlLink);
       });
     }
 
